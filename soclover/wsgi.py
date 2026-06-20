d831691c2e2778c37a51e891f20149170035706b
Ankit Goel <ankitgoel616@gmail.com>
2026-06-20 22:46:32 +0100

    Auto-run migrations on startup for Vercel serverless

--- soclover/wsgi.py
+++ soclover/wsgi.py
@@ -1,5 +1,12 @@
 import os
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soclover.settings')
+
+import django
+django.setup()
+
+from django.core.management import call_command
+call_command('migrate', '--no-input')
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
