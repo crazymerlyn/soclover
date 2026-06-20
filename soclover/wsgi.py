e5dd1a784e3330737c716162a67702d506ad83af
Ankit Goel <ankitgoel616@gmail.com>
2026-06-20 23:46:13 +0100

    Remove runtime database migrations from WSGI application startup to reduce cold start latency

--- soclover/wsgi.py
+++ soclover/wsgi.py
@@ -1,16 +1,7 @@
 import os
 
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soclover.settings')
 
-import django
-django.setup()
-
-from django.db import connection
-from django.core.management import call_command
-
-tables = connection.introspection.table_names()
-if 'django_migrations' not in tables:
-    call_command('migrate', '--no-input')
-
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
