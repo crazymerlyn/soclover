3a1a2393229480b684cbe6a24e5181c3e49d1654
Ankit Goel <ankitgoel616@gmail.com>
2026-06-20 23:00:31 +0100

    Fix critical bugs: wsgi migration guard, apiPost error handling, poll guess overwrite

--- soclover/wsgi.py
+++ soclover/wsgi.py
@@ -5,8 +5,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soclover.settings')
 import django
 django.setup()
 
+from django.db import connection
 from django.core.management import call_command
-call_command('migrate', '--no-input')
+
+tables = connection.introspection.table_names()
+if 'django_migrations' not in tables:
+    call_command('migrate', '--no-input')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
